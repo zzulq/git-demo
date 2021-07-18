41ed3d2dbe4c2914310291db98f597e2c19dda8a
zzulq <ieliquan@163.com>
2021-07-18 15:14:14 +0800

    git-demo 1 version

---
+++ src/test/java/com/atguigu/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
