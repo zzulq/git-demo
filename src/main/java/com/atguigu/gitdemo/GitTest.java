812f8f5106f574a0d30ea0a0c640117e69ca295b
zzulq <ieliquan@163.com>
2021-07-18 15:16:58 +0800

    git-demo 2 version

--- src/main/java/com/atguigu/gitdemo/GitTest.java
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("hello beijing");
     }
 }
